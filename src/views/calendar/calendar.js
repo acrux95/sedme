bc249bcb556a37f3faa2c6048b18404296189183
Walter Miguel Diaz Jaramillo <waltz.mx@gmail.com>
2020-08-21 21:14:29 -0500

    start project

---
+++ src/views/calendar/calendar.js
@@ -0,0 +1,7 @@
+//Se importan los estilos a utilizar en la vista
+import '../../utils/stylesGlobal.scss';
+import '../../components/navbar/navbar.scss';
+import '../../components/sidebar/sidebar.scss';
+
+//Tu codigo JS de la vista
+console.log('TESTING')
