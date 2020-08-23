098c4b527a5622500e7964c9aada231c04365950
Walter Miguel Diaz Jaramillo <waltz.mx@gmail.com>
2020-08-23 11:27:13 -0500

    BEM applied to css, user menu, calendar

--- src/views/calendar/calendar.js
+++ src/views/calendar/calendar.js
@@ -1,7 +1,14 @@
 //Se importan los estilos a utilizar en la vista
-import '../../utils/stylesGlobal.scss';
-import '../../components/navbar/navbar.scss';
-import '../../components/sidebar/sidebar.scss';
+import "../../utils/stylesGlobal.scss";
+import "../../components/navbar/navbar.scss";
+import "../../components/sidebar/sidebar.scss";
+import "../../components/calendar/calendar.scss";
 
 //Tu codigo JS de la vista
-console.log('TESTING')
+let toggleBtn = document
+  .querySelector(".sidebar__toggle-btn")
+  .addEventListener("click", toggle);
+
+function toggle() {
+  alert("Proximamente me escondere, no comas ansias");
+}
