b18263c7cf13b31aa7c4dc2164f834d5a443fc95
Oscar Palomino <ing.oscarp1@gmail.com>
2020-09-09 08:04:33 -0500

    update

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -5,12 +5,13 @@ import '../assets/styles/components/Aside.scss';
 const Aside = () => (
   <aside>
     <ul>
-      <li><a href="#">Inicio</a></li>
-      <li><a href="#">Learning Path</a></li>
-      <li><a href="#">Actividades</a></li>
-      <li><a href="#">Calendario</a></li>
-      <li><a href="#">Metas Semanales</a></li>
-      <li><a href="#">Ayuda</a></li>
+      <li><i></i><a href="#">Home</a></li>
+      <li><i></i><a href="#">Learning Path</a></li>
+      <li><i></i><a href="#">Activities</a></li>
+      <li><i></i><a href="#">Calendar</a></li>
+      <li><i></i><a href="#">Goals of the week</a></li>
+      <li><i></i><a href="#">Users</a></li>
+      <li><i></i><a href="#">Help</a></li>
     </ul>
   </aside>
 );
