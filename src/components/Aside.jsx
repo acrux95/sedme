930ec5aea8587d5be94dbdf400a2b830e7688bf4
Oscar Palomino <ing.oscarp1@gmail.com>
2020-09-16 14:45:18 -0500

    aside corregido

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -4,9 +4,12 @@ import '../assets/styles/components/Aside.scss';
 
 const Aside = () => (
   <aside>
+    <link to='/'>
+      <h1>SEDME</h1>
+    </link>
     <ul>
       <li><a href="#"><i className="fas fa-home"></i><p>Home</p></a></li>
-      <li><a href="#"><i className="fas fa-clipboard-list"></i><p>Learning Pat</p>h</a></li>
+      <li><a href="#"><i className="fas fa-clipboard-list"></i><p>Learning Path</p></a></li>
       <li><a href="#"><i className="fas fa-hat-wizard"></i><p>Activities</p></a></li>
       <li><a href="#"><i className="far fa-calendar-alt"></i><p>Calendar</p></a></li>
       <li><a href="#"><i className="fas fa-chart-bar"></i><p>Goals of the week</p></a></li>
