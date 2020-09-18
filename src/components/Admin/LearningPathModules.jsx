53a8ba3d23f9671d3925a668c191047a205ab820
Oscar Palomino <ing.oscarp1@gmail.com>
2020-09-18 17:32:25 -0500

    learning path modules update

---
+++ src/components/Admin/LearningPathModules.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/components/LearningPathModules.scss';
+
+const LearningPathModules = () => (
+
+
+);
+
+export default LearningPathModules;
