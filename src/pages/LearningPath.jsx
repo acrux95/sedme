5d75b234690c46f0be4bc91ca85699274ca12678
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-10-07 15:39:42 -0500

    Fixing styles

--- src/pages/LearningPath.jsx
+++ src/pages/LearningPath.jsx
@@ -13,11 +13,11 @@ const LearningPath = () => (
             
             <div className="courses-container">
                 <div className="course-box">
-                    <img src="html.png" alt="Cursos del módulo" />
+                    {/* <img src="" alt="Cursos del módulo" /> */}
                     <ul>
                         <li>Diseño de interfaces UI y UX</li>
                         <li>Prototipado</li>
-                        <li>Introducción HMTL y CSS</li>
+                        <li>Introducción HTML y CSS</li>
                     </ul>
                     <div className="btn-container">
                         <a href="#" className="btn">Module: 20 Hours</a>
