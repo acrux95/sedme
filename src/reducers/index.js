fc1e250ea8c0d2f8401994796e928d64566fcdcd
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-10-08 12:43:02 -0500

    working sedme

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -39,12 +39,9 @@ export const GlobalState = (props) => {
     </GlobalContext.Provider>
   );
 };
-<<<<<<< HEAD
-||||||| 5d75b23
 
-=======
 // const reducer = (state, action) => {
 //     switch(action.type)
 // };
 export default reducer;
->>>>>>> master
+
